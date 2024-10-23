317f6afc5af478f3822418f346b3a08b2d3c40bc
Morten Reintz <morten@ThinkPad-X1-Carbon.lan>
2024-10-23 18:39:19 +0200

    Knapper og slikt. Fikset første rad.

--- fretboard_app.py
+++ fretboard_app.py
@@ -3,11 +3,17 @@ from fretboard_ui import Ui_MainWindow
 from fretboard import Fretboard
 from musthe import *
 import sys
+import pandas as pd
 
-def populateFretboard(ui, fretboard):
+def populateFretboard(ui, fretboard, frets):
 
     labels = []
-        
+
+    if min(frets) == 0:
+        fret_slice = range(1, frets[1]+1)
+        df = pd.DataFrame(fretboard)
+        fretboard = df[ fret_slice ].values.tolist()
+
     i = 0
     for row in fretboard:
         j = 0
@@ -22,7 +28,24 @@ def populateFretboard(ui, fretboard):
 
     for row in labels:
         for label in row:
-            label.setAlignment(QtCore.Qt.AlignCenter)
+            if label.text() != '':
+                label.setAlignment(QtCore.Qt.AlignCenter)
+                label.setFrameShape(QtWidgets.QFrame.Box)
+                label.setLineWidth(3)
+
+    fretButtons = []
+
+    j = 0
+    for fret in range(frets[0], frets[1]+1):
+        if fret > 0:
+            button = QtWidgets.QPushButton(ui.gridLayoutWidget)
+            button.setMinimumSize(QtCore.QSize(40, 40))
+            button.setMaximumSize(QtCore.QSize(40, 40))
+            button.setObjectName("fretButton" + str(fret))
+            button.setText(str(fret))
+            ui.gridLayout.addWidget(button, 6, j+1, 1, 1)
+            fretButtons.append(button)
+            j = j + 1
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -34,9 +57,10 @@ if __name__ == "__main__":
     tuning = ['E', 'A', 'D', 'G', 'B', 'E']
 
     f = Fretboard(tuning)
-    notes, intervals = f.build(scale=Scale(Note('A'), 'natural_minor'))
+    frets = (5, 9)
+    notes, intervals = f.build(scale=Scale(Note('A'), 'natural_minor'), frets=frets)
 
-    populateFretboard(ui, notes)
+    populateFretboard(ui, notes, frets)
 
     MainWindow.show()
     sys.exit(app.exec_())
