f9b423d83d254ab927838bd72cbcc479c72152c4
Morten Reintz <morten.reintz@nord.no>
2024-10-24 10:05:08 +0200

    Lagt til masse GUIkomponenter.

--- fretboard_app.py
+++ fretboard_app.py
@@ -5,10 +5,17 @@ from musthe import *
 import sys
 import pandas as pd
 
+def translate(string):
+    string=string.replace('b', '♭')
+    string=string.replace('#', '♯')
+    return string
+
 def populateFretboard(ui, fretboard, frets):
 
+    # Set up all the labels with notes or intervals on them
     labels = []
 
+    # We don't want to show the tuning after the nut as a fret, so we cut that column out.
     if min(frets) == 0:
         fret_slice = range(1, frets[1]+1)
         df = pd.DataFrame(fretboard)
@@ -19,7 +26,7 @@ def populateFretboard(ui, fretboard, frets):
         j = 0
         labelRow = []
         for column in row:
-            label = QtWidgets.QLabel(ui.gridLayoutWidget, text=column)
+            label = QtWidgets.QLabel(ui.centralwidget, text=translate(column))
             ui.gridLayout.addWidget(label, i, j+1, 1, 1)
             labelRow.append(label)
             j = j + 1
@@ -33,12 +40,13 @@ def populateFretboard(ui, fretboard, frets):
                 label.setFrameShape(QtWidgets.QFrame.Box)
                 label.setLineWidth(3)
 
+    # Set up the fret buttons
     fretButtons = []
 
     j = 0
     for fret in range(frets[0], frets[1]+1):
         if fret > 0:
-            button = QtWidgets.QPushButton(ui.gridLayoutWidget)
+            button = QtWidgets.QPushButton(ui.centralwidget)
             button.setMinimumSize(QtCore.QSize(40, 40))
             button.setMaximumSize(QtCore.QSize(40, 40))
             button.setObjectName("fretButton" + str(fret))
@@ -55,10 +63,11 @@ if __name__ == "__main__":
     ui.tuning_1.setFocus()
 
     tuning = ['E', 'A', 'D', 'G', 'B', 'E']
+    frets = (5,9)
 
     f = Fretboard(tuning)
-    frets = (5, 9)
-    notes, intervals = f.build(scale=Scale(Note('A'), 'natural_minor'), frets=frets)
+    notes, intervals = f.build(scale=Scale(Note('Cb'), 'major'), frets=frets)
+    #notes, intervals = f.build(chord=Chord(Note('Cb'), 'dim'))
 
     populateFretboard(ui, notes, frets)
 
