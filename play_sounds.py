d7fcc73cf50ca2859e409373e0e3fe5861ee355d
Morten Reintz <morten.reintz@nord.no>
2024-12-20 09:30:53 +0100

    Embedding synth file in resource file.

--- play_sounds.py
+++ play_sounds.py
@@ -1,9 +1,20 @@
 import time
+import os
 import tinysoundfont
 from musthe import Note, Chord, Scale
+from PyQt5.QtCore import QFile
+import play_sounds_resources
+
+synthfilename = "florestan-piano.sf2"
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+synthfile = os.path.join(__location__, synthfilename)
+
+if not os.path.isfile(synthfile):
+    print("Exporting synth file")
+    QFile.copy(":/resources/florestan-piano.sf2", synthfile)
 
 synth = tinysoundfont.Synth()
-sfid = synth.sfload("florestan-piano.sf2")
+sfid = synth.sfload(synthfile)
 synth.program_select(0, sfid, 0, 0)
 synth.start()
 
